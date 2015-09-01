b0454e433448801ac424ea325b29d7fd64322a6e
liyanpeng5 <liyanpeng5@wanda.cn>
2015-09-02 06:57:41 +0800

    Fix the compilition error.

--- src/main/java/com/robert/httphelper/sample/HttpService.java
+++ src/main/java/com/robert/httphelper/sample/HttpService.java
@@ -3,9 +3,9 @@ package com.robert.httphelper.sample;
 // @RestUrl(url = "http://localhost:8080/student")
 public interface HttpService {
 	// @Get
-	public @Json2Object Student getStudent(@QueryParam long dogId);
+	//public @Json2Object Student getStudent(@QueryParam long dogId);
 	
 	// @Post
-	public @Json2Status int getStudent(@FormField long dogId);
+	//public @Json2Status int getStudent(@FormField long dogId);
 
 }
